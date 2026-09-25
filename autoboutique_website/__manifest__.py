5ee59421537825df5109eda3c5463fe760a56013
Prime Auto Boutique <odoo@autoboutique.local>
2026-09-25 22:40:46 +0800

    Expose Autoboutique website module in Apps

--- autoboutique_website/__manifest__.py
+++ autoboutique_website/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Autoboutique Website",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "summary": "Dedicated premium vehicle storefront for Autoboutique",
     "author": "Autoboutique",
     "category": "Website",
@@ -15,6 +15,6 @@
         ],
     },
     "post_init_hook": "post_init_hook",
-    "application": False,
+    "application": True,
     "installable": True,
 }
