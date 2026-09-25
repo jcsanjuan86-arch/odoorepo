73a0e6e995c0a6234459763b7e6ae391b11414af
Prime Auto Boutique <odoo@autoboutique.local>
2026-09-25 22:34:10 +0800

    Add dedicated Autoboutique multi-website storefront

---
+++ autoboutique_website/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Autoboutique Website",
+    "version": "19.0.1.0.0",
+    "summary": "Dedicated premium vehicle storefront for Autoboutique",
+    "author": "Autoboutique",
+    "category": "Website",
+    "license": "LGPL-3",
+    "depends": ["website_sale", "autoboutique_ops"],
+    "data": [
+        "views/autoboutique_pages.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "autoboutique_website/static/src/scss/autoboutique.scss",
+        ],
+    },
+    "post_init_hook": "post_init_hook",
+    "application": False,
+    "installable": True,
+}
